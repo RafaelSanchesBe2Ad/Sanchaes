a91408e0cfb7b2e43e3b9f569904660f837fad47
sanches <comercial_be2ad@outlook.com>
2023-02-21 15:44:54 -0300

    Atualização de Header, Footer e Pagina de Estudos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { HomeComponent } from './home/home.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AboutComponent } from './about/about.component';
-import { InicioComponent } from './about/inicio/inicio.component';
 import { EstudosComponent } from './about/estudos/estudos.component';
+import { InicioComponent } from './about/inicio/inicio.component';
 import { SucessoComponent } from './about/sucesso/sucesso.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
